f3f79f38bc97aae1fdf70fd6015d8ba811a90c14
Victor K <dev@victorkarasev.ru>
2023-04-10 07:25:03 +0700

    done refactoring

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
 navigator.getBattery().then((battery) => {
-  battery.onchargingchange = chargingChange(battery);
-  battery.onlevelchange = level(battery);
+  battery.onchargingchange = chargingChangeHandler(battery);
+  battery.onlevelchange = levelChangeHandler(battery);
 });
 
-// document.querySelector('#remaining_time').innerHTML = battery.dischargingTime?"Discharged in "+time(battery.dischargingTime)+"s":"Discharged in "+time(battery.dischargingTime)+"s";
-
-function chargingChange(battery) {
+const chargingChangeHandler = (battery) => {
   if (!battery) return;
-  if (battery.charging) {
-    document.querySelector('#battery_bar').style.background = "lightgreen";
-    document.querySelector('#charge_level').style.textShadow = "#029200 0px 0px 50px";
-    document.querySelector('#charging_status').style.textShadow = "#029200 0px 0px 50px";
-    document.querySelector('#charging_status').innerHTML = "charging";
-  } else {
-    // document.querySelector('#battery_bar').style.background = "lightblue";
-    // document.querySelector('#charging_status').style.textShadow = "#001892 0px 0px 50px";
-    document.querySelector('#battery_bar').style.background = "#ff6161";
-    document.querySelector('#charge_level').style.textShadow = "#ff0000 0px 0px 50px";
-    document.querySelector('#charging_status').style.textShadow = "#ff0000 0px 0px 50px";
-    document.querySelector('#charging_status').innerHTML = "discharging";
-  }
-  level(battery);
+  const text = battery.charging ? 'charging' : 'discharging';
+  document.getElementById('charging_status').innerHTML = text;
+  document.getElementById('battery').setAttribute('data-charging', battery.charging)
 }
 
-function level(battery) {
+const levelChangeHandler = (battery) => {
   if (!battery) return;
-  document.querySelector('#charge_level').innerHTML = Math.round(battery.level*100)+'%';
-  var par = Math.floor(window.getComputedStyle(document.querySelector('#battery')).height.replace('px',''))/100
-  document.querySelector('#battery_bar').style.height = par*(battery.level*100)+'px';
-}
-
-function time(sec) {
-  var s, m, h, d;
-  if (sec>=60 && sec<3600) {
-    s = sec%60;
-    m = (sec-s)/60;
-    return m+'m '+s;
-  } else if (sec>=3600 && sec<86400) {
-    s = sec%60;
-    m = (sec-s)/60%60
-    h = Math.floor((sec-s)/3600);
-    return h+'h '+m+'m '+s;
-  } else if (sec>=86400) {
-    s = sec%60;
-    m = (sec-s)/60%60
-    h = Math.floor((sec-s)/3600%24);
-    d = Math.floor((sec-s)/86400);
-    return d+'d '+h+'h '+m+'m '+s;
-  } else {
-    return sec;
-  }
+  const { offsetHeight: fullHeigth } = document.getElementById('battery');
+  document.getElementById('charge_level').innerHTML = `${Math.round(battery.level * 100)}%`;
+  document.getElementById('battery_bar').style.height = `${fullHeigth * battery.level}px`;
 }
