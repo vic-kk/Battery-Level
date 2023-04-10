9670a1decba1be21c5b226c2a2a76fe7d8a11042
Victor K <36292917+vikdubber@users.noreply.github.com>
2023-04-11 05:35:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ navigator.getBattery().then((battery) => {
   battery.onchargingchange = () => chargingChangeHandler(battery);
   battery.onlevelchange = () => levelChangeHandler(battery);
   
+  firstUpdate(battery);
+});
+
+const firstUpdate = (battery) => {
   chargingChangeHandler(battery);
   levelChangeHandler(battery);
-});
+}
 
 const chargingChangeHandler = (battery) => {
   if (!battery) return;
-  const text = battery.charging ? 'charging' : 'discharging';
+  const { charging: isCharging } = battery;
+  const text = isCharging ? 'charging' : 'discharging';
   document.getElementById('charging_status').innerHTML = text;
-  document.getElementById('battery').setAttribute('data-charging', battery.charging)
+  document.getElementById('battery').setAttribute('data-charging', isCharging)
 }
 
 const levelChangeHandler = (battery) => {
   if (!battery) return;
+  const { level: chargeLevel } = battery;
   const { offsetHeight: fullHeigth } = document.getElementById('battery');
-  document.getElementById('charge_level').innerHTML = `${Math.round(battery.level * 100)}%`;
-  document.getElementById('battery_bar').style.height = `${fullHeigth * battery.level}px`;
+  document.getElementById('charge_level').innerHTML = `${Math.round(chargeLevel * 100)}%`;
+  document.getElementById('battery_bar').style.height = `${fullHeigth * chargeLevel}px`;
 }
